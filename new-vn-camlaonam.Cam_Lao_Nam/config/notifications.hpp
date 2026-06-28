730fe2369e8863b3de6f6474318d82f46a18ce6c
Logiztiic <icodehtml554@gmail.com>
2026-06-28 11:53:30 -0400

    Add files via upload

---
+++ new-vn-camlaonam.Cam_Lao_Nam/config/notifications.hpp
@@ -0,0 +1,80 @@
+class CfgNotifications {
+    class TrainingComplete {
+        title = "Training Complete";
+        iconPicture = "images\checkmark.paa";
+        description = "%1";
+        duration = 3;
+    };
+    class AccessDenied {
+        title = "Access Denied";
+        iconPicture = "images\xmark.paa";
+        description = "%1";
+        duration = 3;
+    };
+    class ReconUpdate {
+        title = "Recon";
+        iconPicture = "images\recon.paa";
+        description = "%1";
+        duration = 2;
+    };
+    class StationShutdown {
+        title = "Station Destroyed";
+        iconPicture = "images\stationteardown.paa";
+        description = "%1";
+        duration = 5;
+    };
+    class StationStartup {
+        title = "Station Created";
+        iconPicture = "images\stationteardown.paa";
+        description = "%1";
+        duration = 5;
+    };
+    class StationDestroyed {
+        title = "Station Destroyed";
+        iconPicture = "images\stationteardown.paa";
+        description = "%1";
+        duration = 5;
+    };
+    class RoleMedicIcon {
+        title = "Medic";
+        iconPicture = "images\medic.paa";
+        description = "%1";
+        duration = 3;
+    };
+    class RoleEngineerIcon {
+        title = "Engineer";
+        iconPicture = "images\engineer.paa";
+        description = "%1";
+        duration = 3;
+    };
+    class RoleConstructionIcon {
+        title = "Construction";
+        iconPicture = "images\construction.paa";
+        description = "%1";
+        duration = 3;
+    };
+    class NoIntel {
+        title = "Intel Update";
+        iconPicture = "images\xmark.paa";
+        description = "%1";
+        duration = 2;
+    };
+    class YesIntel {
+        title = "Intel Update";
+        iconPicture = "images\checkmark.paa";
+        description = "%1";
+        duration = 2;
+    };
+    class Fail {
+        title = "Failed";
+        iconPicture = "images\xmark.paa";
+        description = "%1";
+        duration = 2;
+    };
+    class Success {
+        title = "Success";
+        iconPicture = "images\checkmark.paa";
+        description = "%1";
+        duration = 2;
+    };
+};
